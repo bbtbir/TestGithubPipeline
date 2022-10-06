27ee8bc330311f73ac3767a2a6af0afc04f5929f
Sarah Birrer <sarah.birrer@bbtsoftware.ch>
2022-10-06 10:26:29 +0200

    use cake to build

--- Program.cs
+++
@@ -1,3 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-string message = "Hello, Universe!";
-Console.WriteLine(message);
---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace TestGithubPipeline;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string message = "Hello, Universe";
+        Console.WriteLine(message);
+    }
+}
